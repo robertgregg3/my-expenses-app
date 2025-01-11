0945572b720d0d637abda888d30eaf76dbdaa20c
Robert <iamrobertgregg@gmail.com>
2025-01-11 13:04:21 +0000

    initial commit

---
+++ src/main.js
@@ -0,0 +1,3 @@
+const heading = document.querySelector('h1');
+
+heading.textContent = 'No, no no...Hello, World!';
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    build: {
+        outDir: './my-craftcms-app/web/assets',
+    },
+    root: 'src',
+});
